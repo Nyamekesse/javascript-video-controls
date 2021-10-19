07c3eeac7f9481b8167c77a847fd22db82b3c175
Samuel Nyamekesse <snyamekesse@stu.ucc.edu.gh>
2021-10-19 14:51:41 +0100

    Completed addeding all functionalities

--- video.js
+++ video.js
@@ -1,19 +1,38 @@
-const btn = document.querySelectorAll('.video-switch span');
+const btn = document.querySelectorAll('span');
 const videoBox = document.querySelector ('.video-container');
+
 
 
 btn.forEach((button)=> {
     button.addEventListener('click', (e)=> {;
+        const updater = e.currentTarget;
+        updater.classList.add('active');
+    
+        btn.forEach((element)=> {
+            element.addEventListener('click', (e)=> {
+                let currentBtn = e.currentTarget;
+                if (updater !== currentBtn) {
+                    updater.classList.remove('active');
+                }
+            });
+    
+        });
+
         const currentState = e.currentTarget.dataset.id;
-       
         if (currentState === "pause") {
             videoBox.pause();
         }
-        else {
+        else if(currentState === "play"){
             videoBox.play();
         }
 
     });
-    
-    
+
 });
+
+// preloader
+const loader = document.querySelector('.preloader-container');
+
+window.addEventListener('load', ()=> {
+    loader.classList.add('hidden');
+})
